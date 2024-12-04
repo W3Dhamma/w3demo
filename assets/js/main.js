776c2c65eafd8738f8a0feb3348d9d4762c8cc1c
W3Dhamma <w3dhamma.in@gmail.com>
2024-12-04 12:46:17 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,7 @@ import './search.js';
 import './animations.js';
 import './contactForm.js';
 import './particles.js';
+import './humberger.js';
 
 // Any additional initialization or global code can go here
 console.log('W3Dhamma JavaScript initialized');
